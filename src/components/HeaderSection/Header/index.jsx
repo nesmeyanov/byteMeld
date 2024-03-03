5e300c36834ba1489461da936828521f64568f6c
Slava Pivovarov <sllawwkoo@gmail.com>
2024-03-03 17:58:11 +0200

    UPDATE: component Header, header.module.scss, App.scss; added image bg and helpers.js

--- src/components/HeaderSection/Header/index.jsx
+++ src/components/HeaderSection/Header/index.jsx
@@ -1,16 +1,28 @@
 import style from "./header.module.scss";
 import { Link } from "react-router-dom";
 import { Logo } from "../../Icons/logo";
+import { useContext } from "react";
+import { BreakPoint } from "../../../router";
+import {scrollUpPage} from "../../../utils/helpers"
+import { useTranslation } from "react-i18next";
 
 export function Header() {
+const{t} = useTranslation()
+const isMobil = useContext(BreakPoint)
 
 	return (
 		<header className={style.header}>
 			<div className={style.container}>
-				<Link to='/'>
+				<Link
+					to="/"
+					className={style.logo}
+					onClick={scrollUpPage}
+				>
 					<Logo />
 				</Link>
-
+				<div className={style.actions}>
+					{!isMobil && <button className={style.btn_header}>{t("header.btn")}</button>}
+				</div>
 			</div>
 		</header>
 	);
---
+++ src/utils/helpers.js
@@ -0,0 +1,12 @@
+export const scrollTo = (start) => {
+	const section = document.querySelector(start);
+	const coord =
+		section.offsetTop === 0
+			? window.scrollY - Math.abs(section.getBoundingClientRect().top) - 50
+			: section.offsetTop - 50;
+	window.scrollTo({ top: coord, behavor: "smooth" });
+};
+
+export const scrollUpPage = () => {
+	window.scrollTo({ top: 0, behavor: "smooth" });
+};
