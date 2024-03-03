463daa17d8158d0790765cd592cc957a35e83dfd
Slava Pivovarov <sllawwkoo@gmail.com>
2024-03-03 13:38:50 +0200

    CREATE: base component Header and header.module.scss

---
+++ src/components/HeaderSection/Header/index.jsx
@@ -0,0 +1,11 @@
+import style from './header.module.scss';
+
+export function Header() {
+	return (
+		<header className={style.header}>
+			<div className={style.container}>
+
+			</div>
+		</header>
+	)
+}
