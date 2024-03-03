d42e55ce036d80d1daf1ce7b01813a8d83885663
Slava Pivovarov <sllawwkoo@gmail.com>
2024-03-03 13:44:38 +0200

    CREATE: base component Footer and footer.module.scss

---
+++ src/components/FooterSection/Footer/index.jsx
@@ -0,0 +1,11 @@
+import style from "./footer.module.scss";
+
+export function Footer() {
+	return (
+		<footer className={style.footer}>
+			<div className={style.container}>
+
+			</div>
+		</footer>
+	)
+}
