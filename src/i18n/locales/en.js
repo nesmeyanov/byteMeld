81ffdbae4ebe30f5aaaaf917579671143d561a4d
Slava Pivovarov <sllawwkoo@gmail.com>
2024-03-03 13:13:10 +0200

    UPDATE: locales files uk, en

--- src/i18n/locales/en.js
+++ src/i18n/locales/en.js
@@ -1,1 +1,35 @@
-export const en = {}
+export const en = {
+	//***Header***//
+	header: {
+		menu: {
+			home: {
+				title: "Home",
+			},
+			about: {
+				title: "About us",
+				anchor: "#about",
+			},
+			service: {
+				title: "Service",
+				anchor: "#service",
+			},
+			portfolio: {
+				title: "Portfolio",
+				anchor: "#portfolio",
+			},
+			reviews: {
+				title: "Reviews",
+				anchor: "#reviews",
+			},
+			blog: {
+				title: "Blog",
+				anchor: "#blog",
+			},
+			contacts: {
+				title: "Contacts",
+				anchor: "#contacts",
+			},
+		},
+		btn: "Contact us",
+	},
+};
--- src/i18n/locales/uk.js
+++ src/i18n/locales/uk.js
@@ -1,1 +1,35 @@
-export const uk = {}
+export const uk = {
+	//***Header***//
+	header: {
+		menu: {
+			home: {
+				title: "Головна",
+			},
+			about: {
+				title: "Про нас",
+				anchor: "#about",
+			},
+			service: {
+				title: "Послуги",
+				anchor: "#service",
+			},
+			portfolio: {
+				title: "Портфоліо",
+				anchor: "#portfolio",
+			},
+			reviews: {
+				title: "Відгуки",
+				anchor: "#reviews",
+			},
+			blog: {
+				title: "Блог",
+				anchor: "#blog",
+			},
+			contacts: {
+				title: "Контакти",
+				anchor: "#contacts",
+			},
+		},
+		btn: "Зв'яжіться з нами",
+	},
+};
