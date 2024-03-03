f5942bb3bad18c4b7771e57399a01e624ed7d267
Slava Pivovarov <sllawwkoo@gmail.com>
2024-03-03 15:21:43 +0200

    UPDATE: router and added context break point media

--- src/router/index.jsx
+++ src/router/index.jsx
@@ -1,18 +1,26 @@
 import { Route, Routes } from "react-router-dom";
 import { MainLayout, Home } from "../pages";
+import { createContext } from "react";
+import { useMediaQuery } from "react-responsive";
+export const BreakPoint = createContext();
 
 export default function Router() {
+	// прапор для компонентів, щоб не рендерити іх на мобільних застосунках
+	const isMobile = useMediaQuery({ maxWidth: 991 });
+	
 	return (
-		<Routes>
-			<Route
-				path="/"
-				element={<MainLayout />}
-			>
+		<BreakPoint.Provider value={isMobile}>
+			<Routes>
 				<Route
-					index
-					element={<Home />}
-				/>
-			</Route>
-		</Routes>
+					path="/"
+					element={<MainLayout />}
+				>
+					<Route
+						index
+						element={<Home />}
+					/>
+				</Route>
+			</Routes>
+		</BreakPoint.Provider>
 	);
 }
