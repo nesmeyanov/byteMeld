29c2c10d75498e20c397edf88fac1a30f1b28b00
Slava Pivovarov <sllawwkoo@gmail.com>
2024-03-03 16:59:49 +0200

    CREATE: icons Logo

--- src/components/HeaderSection/Header/index.jsx
+++ src/components/HeaderSection/Header/index.jsx
@@ -1,10 +1,17 @@
 import style from "./header.module.scss";
+import { Link } from "react-router-dom";
+import { Logo } from "../../Icons/logo";
 
 export function Header() {
 
 	return (
 		<header className={style.header}>
-			<div className={style.container}></div>
+			<div className={style.container}>
+				<Link to='/'>
+					<Logo />
+				</Link>
+
+			</div>
 		</header>
 	);
 }
