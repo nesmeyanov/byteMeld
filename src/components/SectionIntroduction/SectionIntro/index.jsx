9281ec11022bec10ccce8a31769a9e3c07067534
Slava Pivovarov <sllawwkoo@gmail.com>
2024-03-05 00:12:13 +0200

    CREATE: component SectionIntro and sectionIntro.module.scss

---
+++ src/components/SectionIntroduction/SectionIntro/index.jsx
@@ -0,0 +1,12 @@
+import style from "./sectionIntro.module.scss"
+
+export function SectionIntro() {
+
+	return (
+		<section className={style.wrapperIntro}>
+			<div className={style.container}>
+				
+			</div>
+		</section>
+	)
+}
