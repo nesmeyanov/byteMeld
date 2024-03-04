f3d4ffb1631526d0e2e965600283720d887d6d57
Slava Pivovarov <sllawwkoo@gmail.com>
2024-03-04 15:13:16 +0200

    CREATE: component BurgerMenu and burgerMenu.module.scss

---
+++ src/components/HeaderSection/BurgerMenu/index.jsx
@@ -0,0 +1,13 @@
+import { motion } from "framer-motion"
+import style from "./burgerMenu.module.scss"
+
+export function BurgerMenu() {
+
+	return (
+		<motion.div className={style.wrapper}>
+			<motion.div className={style.container}>
+				
+			</motion.div>
+		</motion.div>
+	)
+}
