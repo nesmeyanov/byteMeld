45f6ea4863075c4604cb084e8a3376e82e393ee9
Slava Pivovarov <sllawwkoo@gmail.com>
2024-03-06 01:18:46 +0200

    UPDATE: locales files

--- src/i18n/locales/en.js
+++ src/i18n/locales/en.js
@@ -37,4 +37,22 @@ export const en = {
 	intro: {
 		text: "We develop innovative solutions in web development",
 	},
+
+	///*** SectionAboutUs ***/
+	about: {
+		text1:
+			"We are a creative team of professionals who are dedicated to their work and passionate about achieving results.",
+		text2:
+			"Our mission is to create innovative solutions that change the world around us.",
+		quality: {
+			desc1:
+				"We are always improving, introducing new technologies and approaches to provide your company with a competitive advantage.",
+			desc2:
+				"We are proud of our long-term relationships with our clients and our reputation as a reliable and trusted partner.",
+			desc3:
+				"We focus on delivering concrete results for our clients and work to achieve their business goals.",
+			desc4:
+				"Our team is always ready to help you solve any questions or problems and provide the best support service.",
+		},
+	},
 };
