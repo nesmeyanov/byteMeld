8b61233ad5c888c4970f51896c6514301e76d897
Slava Pivovarov <sllawwkoo@gmail.com>
2024-03-06 01:45:40 +0200

    CREATE: icon componets OpenTag and CloseTag

--- src/components/Icons/index.js
+++ src/components/Icons/index.js
@@ -1,3 +1,5 @@
 export { Logo } from "./logo";
 export { ArrowDown } from "./arrow-down";
 export { ArrowRight } from "./arrow-right";
+export {OpenTag} from "./open-tag.jsx";
+export {CloseTag} from "./close-tag.jsx";
