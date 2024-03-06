ed4fc956221be83330730bec461a3ee21e73f228
Slava Pivovarov <sllawwkoo@gmail.com>
2024-03-06 10:29:13 +0200

    UPDATE: page Home

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,9 +1,11 @@
+import { SectionAbout } from "../../components/SectionAboutUs/SectionAbout";
 import { SectionIntro } from "../../components/SectionIntroduction/SectionIntro";
 
 export function Home() {
 	return (
 		<div id="main">
 			<SectionIntro />
+			<SectionAbout />
 		</div>
-	)
+	);
 }
