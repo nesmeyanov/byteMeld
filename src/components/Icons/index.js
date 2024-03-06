40aee42baa9725c61549a71a5000e8b0342fb95a
Slava Pivovarov <sllawwkoo@gmail.com>
2024-03-06 10:18:48 +0200

    CREATE: icon components Chat, Check, Cpu, Target

--- src/components/Icons/index.js
+++ src/components/Icons/index.js
@@ -1,5 +1,9 @@
 export { Logo } from "./logo";
 export { ArrowDown } from "./arrow-down";
 export { ArrowRight } from "./arrow-right";
-export {OpenTag} from "./open-tag.jsx";
-export {CloseTag} from "./close-tag.jsx";
+export { OpenTag } from "./open-tag.jsx";
+export { CloseTag } from "./close-tag.jsx";
+export { Cpu } from "./cpu.jsx";
+export { Check } from "./check.jsx";
+export { Target } from "./target.jsx";
+export {Chat} from "./chat.jsx"
