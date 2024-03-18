e026b82b9cf2a03f7639edde775140329f64d8a0
Slava Pivovarov <sllawwkoo@gmail.com>
2024-03-18 17:25:19 +0200

    UPDATE: page MainLayout

--- src/pages/MainLayout/index.jsx
+++ src/pages/MainLayout/index.jsx
@@ -1,12 +1,14 @@
 import { Outlet } from "react-router-dom";
 import { Header } from "../../components/HeaderSection/Header";
-import { Footer} from "../../components/FooterSection/Footer";
+import { Footer } from "../../components/FooterSection/Footer";
+import BackToTopButton from "../../components/ButtonToTop";
 
 export function MainLayout() {
 	return (
 		<>
 			<Header />
 			<Outlet />
+			<BackToTopButton />
 			<Footer />
 		</>
 	);
