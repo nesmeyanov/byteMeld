4289b287c0664f014445c2229193e91535301dd2
Slava Pivovarov <sllawwkoo@gmail.com>
2024-03-18 16:43:43 +0200

    UPDATE: change horizontal animation to vertical

--- src/utils/animation/introAnimation.js
+++ src/utils/animation/introAnimation.js
@@ -1,29 +1,3 @@
-// export const borderAnimation = (index) => ({
-// 	hidden: { borderBottomColor: "#E6F5FF" },
-// 	visible: {
-// 		borderBottomColor: "#21A2FF",
-// 		transition: { delay: 0.25 + index * 0.2, duration: 0.4, ease: "easeOut" },
-// 	},
-// });
-
-// export const charAnimation = (index) => ({
-// 	hidden: { opacity: 1, x: 0 },
-// 	visible: {
-// 		opacity: 0,
-// 		x: 300,
-// 		transition: { delay: 0.5 + index * 0.1, duration: 0.5, ease: "easeOut" },
-// 	},
-// });
-
-// export const wordAnimation = (index) => ({
-// 	hidden: { opacity: 0, x: -300 },
-// 	visible: {
-// 		opacity: 1,
-// 		x: 0,
-// 		transition: { delay: 0.5 + index * 0.1, duration: 0.5, ease: "easeOut" },
-// 	},
-// });
-
 export const borderAnimation = (index) => ({
 	hidden: {
 		borderBottomColor: "#E6F5FF",
@@ -38,11 +12,37 @@ export const borderAnimation = (index) => ({
 	},
 });
 
+// export const charAnimation = (index) => ({
+// 	hidden: { opacity: 1, x: 0 },
+// 	visible: {
+// 		opacity: 0,
+// 		x: 300,
+// 		transition: {
+// 			delay: 0.01 + index * 0.1,
+// 			duration: 0.2 + index * 0.1,
+// 			ease: "easeOut",
+// 		},
+// 	},
+// });
+
+// export const wordAnimation = (index) => ({
+// 	hidden: { opacity: 0, x: -300 },
+// 	visible: {
+// 		opacity: 1,
+// 		x: 0,
+// 		transition: {
+// 			delay: 0.01 + index * 0.1,
+// 			duration: 0.2 + index * 0.1,
+// 			ease: "easeOut",
+// 		},
+// 	},
+// });
+
 export const charAnimation = (index) => ({
-	hidden: { opacity: 1, x: 0 },
+	hidden: { opacity: 1, y: 0 },
 	visible: {
 		opacity: 0,
-		x: 300,
+		y: 300,
 		transition: {
 			delay: 0.01 + index * 0.1,
 			duration: 0.2 + index * 0.1,
@@ -52,10 +52,10 @@ export const charAnimation = (index) => ({
 });
 
 export const wordAnimation = (index) => ({
-	hidden: { opacity: 0, x: -300 },
+	hidden: { opacity: 0, y: -300 },
 	visible: {
 		opacity: 1,
-		x: 0,
+		y: 0,
 		transition: {
 			delay: 0.01 + index * 0.1,
 			duration: 0.2 + index * 0.1,
