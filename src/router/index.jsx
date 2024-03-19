40c61d4a03c4611c6f0b5109e998b6be9cd9728b
Slava Pivovarov <sllawwkoo@gmail.com>
2024-03-20 00:27:06 +0200

    UPDATE: Route, added two pathes

--- src/router/index.jsx
+++ src/router/index.jsx
@@ -2,12 +2,14 @@ import { Route, Routes } from "react-router-dom";
 import { MainLayout, Home } from "../pages";
 import { createContext } from "react";
 import { useMediaQuery } from "react-responsive";
+import { Blog } from "../pages/Blog";
+import { Article } from "../pages/Article";
 export const BreakPoint = createContext();
 
 export default function Router() {
 	// прапор для компонентів, щоб не рендерити іх на мобільних застосунках
 	const isMobile = useMediaQuery({ maxWidth: 991 });
-	
+
 	return (
 		<BreakPoint.Provider value={isMobile}>
 			<Routes>
@@ -18,6 +20,14 @@ export default function Router() {
 					<Route
 						index
 						element={<Home />}
+					/>
+					<Route
+						path="/blog"
+						element={<Blog />}
+					/>
+					<Route
+						path="/blog/:slug"
+						element={<Article />}
 					/>
 				</Route>
 			</Routes>
