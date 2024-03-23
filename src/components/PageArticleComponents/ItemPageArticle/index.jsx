629e737d6df9b1c4cd82eae1e66adf835964fc32
Slava Pivovarov <sllawwkoo@gmail.com>
2024-03-23 22:54:36 +0200

    UPDATE: PageArticleComponents and style files

--- src/components/PageArticleComponents/ItemPageArticle/index.jsx
+++ src/components/PageArticleComponents/ItemPageArticle/index.jsx
@@ -1,8 +1,8 @@
-import style from "./itemPageArticle.module.scss"
+import style from "./itemPageArticle.module.scss";
 import { formatDate } from "../../../utils/helpers";
+import React from "react";
 
 export function ItemPageArticle({ title, date, thumbnail, blocks }) {
-
 	return (
 		<div className={style.wrapper}>
 			<div className={style.main_box}>
@@ -16,6 +16,30 @@ export function ItemPageArticle({ title, date, thumbnail, blocks }) {
 					<span className={style.date}>{formatDate(date)}</span>
 					<h3 className={style.title}>{title}</h3>
 				</div>
+			</div>
+			<div className={style.bloks_body}>
+				{blocks?.map(({ subtitle, description, image }, index) => (
+					<React.Fragment key={index}>
+						
+						{(subtitle || description) && (
+							<div className={style.subcontent_box}>
+								{subtitle && <h4 className={style.subtitle}>{subtitle}</h4>}
+								{description && (
+									<p className={style.description}>{description}</p>
+								)}
+							</div>
+						)}
+
+						{image && (
+							<div className={style.sub_image}>
+								<img
+									src={image}
+									alt={subtitle}
+								/>
+							</div>
+						)}
+					</React.Fragment>
+				))}
 			</div>
 		</div>
 	);
